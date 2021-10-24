b7e9c82cd2f4bd0ebc7d18792d1c6c9a9b555969
unknown <winniekagendo35@gmail.com>
2021-10-24 22:06:12 +0300

    final commit

--- src/index.server.js
+++ src/index.server.js
@@ -19,12 +19,12 @@ const adminOrderRoute = require("./routes/admin/order.routes");
 
 //environment variable or you can say constants
 env.config();
+const PORT=process.env.PORT ||4000;
 
 // mongodb connection
 //mongodb+srv://root:<password>@cluster0.8pl1w.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.8pl1w.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  .connect("",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -51,5 +51,5 @@ app.use("/api", orderRoutes);
 app.use("/api", adminOrderRoute);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
